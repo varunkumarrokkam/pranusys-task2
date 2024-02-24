0aa8371985263a9c40ea7c6a3b273c3bdae5ed8e
varunkumarrokkam <160482519+varunkumarrokkam@users.noreply.github.com>
2024-02-24 22:32:43 +0530

    Delete factorial_usingternary.js

--- factorial_usingternary.js
+++
@@ -1,9 +0,0 @@
-
-
-function factorial(num) {
-    return num === 0  ||  num  === 1 ?  1  :  num *  factorial(num - 1);
-
-}
-const num = 5;
-const result = factorial(num);
-console.log(`The factorial of number ${num} is ` + result);
